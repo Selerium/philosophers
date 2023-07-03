9e5979cf249bd9c81d3975d07b748d95ab2e9d29
John Adithya <jadithya@42student.abudhabi.ae>
2023-07-03 18:14:03 +0400

    implemented mutex locks for forks, death timer remaining

--- src/run_sim.c
+++ src/run_sim.c
@@ -6,7 +6,7 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 20:05:31 by jadithya          #+#    #+#             */
-/*   Updated: 2023/07/01 22:37:41 by jadithya         ###   ########.fr       */
+/*   Updated: 2023/07/03 18:07:12 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,24 @@ void	set_forks(t_fork forks[2], t_sim *sim, int i)
 	forks[R] = sim->forks[i];
 }
 
+void	lock_forks(t_fork forks[2], int i)
+{
+	struct timeval	tv;
+
+	pthread_mutex_lock(&forks[0].lock);
+	gettimeofday(&tv, NULL);
+	printf("%ld %d has picked up a fork\n", tv.tv_usec / 1000, i + 1);
+	pthread_mutex_lock(&forks[1].lock);
+	gettimeofday(&tv, NULL);
+	printf("%ld %d has picked up a fork\n", tv.tv_usec / 1000, i + 1);
+}
+
+void	unlock_forks(t_fork forks[2])
+{
+	pthread_mutex_unlock(&forks[0].lock);
+	pthread_mutex_unlock(&forks[1].lock);
+}
+
 void	*sayhi(t_sim *sim)
 {
 	struct timeval	tv;
@@ -32,9 +50,11 @@ void	*sayhi(t_sim *sim)
 	while (sim->philos[i].number_of_meals++
 		< sim->number_of_times_each_philosopher_must_eat)
 	{
+		lock_forks(forks, i);
 		gettimeofday(&tv, NULL);
 		printf("%ld %d is eating\n", tv.tv_usec / 1000, i + 1);
 		usleep(sim->time_to_eat * 1000);
+		unlock_forks(forks);
 		gettimeofday(&tv, NULL);
 		printf("%ld %d is sleeping\n", tv.tv_usec / 1000, i + 1);
 		usleep(sim->time_to_sleep * 1000);
