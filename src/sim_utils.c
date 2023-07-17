3daf393561a75e0f0251d0b87a05027ae02db274
John Adithya <jadithya@lab1r1s8.42abudhabi.ae>
2023-07-17 16:09:45 +0400

    fixing merge mistakes

--- src/sim_utils.c
+++ src/sim_utils.c
@@ -6,7 +6,7 @@
 /*   By: jadithya <jadithya@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 11:39:52 by jadithya          #+#    #+#             */
-/*   Updated: 2023/07/17 15:43:30 by jadithya         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:09:29 by jadithya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,16 +30,13 @@ int	check_sim_dead(t_sim *sim, int i)
 	int	val;
 
 	val = 0;
-	if (sim->philos[i].death_timer <= 0)
-	{
-		set_sim_dead(sim, i);
-		return (1);
-	}
+	pthread_mutex_lock(&sim->lock);
 	if (sim->is_dead)
 	{
 		val = 1;
 		sim->philos[i].death_timer = 0;
 	}
+	pthread_mutex_unlock(&sim->lock);
 	return (val);
 }
 
